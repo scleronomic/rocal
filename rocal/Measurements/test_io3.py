44e9c729c53e5e25399911e49381fead3dd2237d
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-07-05 22:46:08 +0200

    save left measurements

--- rocal/Measurements/test_io3.py
+++ rocal/Measurements/test_io3.py
@@ -4,8 +4,8 @@ from mopla.Planner.ardx2 import ardx, pkt2dict
 from rocal.definitions import ICHR22_AUTOCALIBRATION
 #ardx.require("bcatch.imu-to-ard.imu-raw-packets")
 
-#filepath = "/volume/USERSTORE/tenh_jo/Data/Calibration/Kinect/Left/paths_10_kinect-left-1657027137-measurements"
-filepath = "/volume/USERSTORE/tenh_jo/Data/Calibration/Kinect/Right/paths_10_kinect-right_mirror-1657042612-measurements"
+filepath = "/volume/USERSTORE/tenh_jo/Data/Calibration/Kinect/Left/paths_10_kinect-left-1657027137-measurements"
+# filepath = "/volume/USERSTORE/tenh_jo/Data/Calibration/Kinect/Right/paths_10_kinect-right_mirror-1657042612-measurements"
 #filepath = "/volume/USERSTORE/tenh_jo/Data/Calibration/Kinect/Pole/paths_10_kinect-pole-1657033930-measurements"
 
 
@@ -32,31 +32,61 @@ def pkt_list2dict_list(file):
 
 pkt_list2dict_list(file=filepath)
 
-#
-# file = f"{ICHR22_AUTOCALIBRATION}/Measurements/Real/paths_10_kinect-right_mirror-1657042612-measurements.npy"
-#
+from wzk.mpl import new_fig, imshow, save_fig
+
+file = f"{ICHR22_AUTOCALIBRATION}/Measurements/Real/paths_10_kinect-right_mirror-1657042612-measurements.npy"
+
 # d = np.load(file, allow_pickle=True)
 #
+# i = 3
+# img = d[i]['rgb']['img']
+# marker = d[i]['marker']
 #
-# for i in range(40):
-#     img = d[i]['rgb']['img']
-#     marker_detections = d[i]['marker']['detections']
-#     print(d[i]['marker'])
-#     print(marker_detections)
+# # for j in range(10):
+# #     print(j, d[i]['marker']['num'], d[i]['marker']['detections'])
+#
+#
+# def get_marker_u(marker):
+#     if marker['num'] != 1:
+#         return None
+#     else:
+#         return np.array([marker['detections'][0]['y'], marker['detections'][0]['x']])
+#
 #
 # def reshape_img(img):
 #     img = img.reshape(480, 640, 3)
 #     img = np.swapaxes(img, 0, 1)
 #     return img
 #
+#
+# def plot_all_images():
+#     for i in range(len(d)):
+#         img = d[i]['rgb']['img']
+#         img = reshape_img(img=img)
+#         xy = get_marker_u(d[i]['marker'])
+#         fig, ax = new_fig(aspect=1)
+#         plt.imshow(img, origin='lower')
+#         if xy is not None:
+#             plt.plot(*xy, marker='x', color='red', markersize=30, lw=5)
+#         save_fig(fig=fig, file=f"{ICHR22_AUTOCALIBRATION}/{i}_marker", formats='png')
+#
 # img = reshape_img(img=img)
+#
+# fig, ax = new_fig(aspect=1)
+# # imshow(ax=ax, img=img)
+#
+#
 #
 #
 # from matplotlib import pyplot as plt
 #
-# # from wzk.mpl import new_fig, imshow
 #
-# # fig, ax = new_fig(aspect=1)
-# # imshow(ax=ax, img=img)
-#
+# plot_all_images()
+
+
+# from wzk.mpl import new_fig, imshow
+# fig, ax = new_fig(aspect=1)
+# imshow(ax=ax, img=img)
+
 # plt.imshow(img, origin='lower')
+# plt.plot(*get_marker_u(marker), marker='x', color='red', markersize=20)
