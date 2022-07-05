8312429e6af6fb2e42a8a593c9211fbe12ccbea4
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-07-05 21:41:36 +0200

    test loading script

--- rocal/Measurements/test_io3.py
+++ rocal/Measurements/test_io3.py
@@ -1,4 +1,6 @@
-import ardx.ardx as ardx
+import numpy as np
+from mopla.Planner.ardx2 import ardx, pkt2dict
+
 ardx.require("autocalib.detect-marker-ard.marker-detector-result-packets")
 ardx.require("robotfusion.kinect-to-ardx.kinect-packets")
 ardx.require("monitor.torso-monitor-packets")
@@ -8,34 +10,22 @@ ardx.require("monitor.torso-monitor-packets")
 filepath = "/volume/USERSTORE/tenh_jo/Data/Calibration/Kinect/Right/paths_10_kinect-right_mirror-1657042612-measurements"
 #filepath = "/volume/USERSTORE/tenh_jo/Data/Calibration/Kinect/Pole/paths_10_kinect-pole-1657033930-measurements"
 
-rgb = ardx.read_recorder_file(filepath, "rgb-kinect", "kinect_rgb_packet")
-marker = ardx.read_recorder_file(filepath, "marker-rgb-kinect", "MarkerDetectionResultPacket")
-torso = ardx.read_recorder_file(filepath, "torso-monitor", "torso_monitor_packet")
-#imu_base = ardx.read_recorder_file(filepath, "imu-base", "IMURDataPacket")
+
+def pkt_list2dict_list(file):
+    rgb = ardx.read_recorder_file(file, "rgb-kinect", "kinect_rgb_packet")
+    marker = ardx.read_recorder_file(file, "marker-rgb-kinect", "MarkerDetectionResultPacket")
+    torso = ardx.read_recorder_file(file, "torso-monitor", "torso_monitor_packet")
+    base = ardx.read_recorder_file(file, "torso-monitor", "torso_monitor_packet")
+    assert len(rgb) == len(marker) == len(torso) == len(base)
+
+    d = []
+    for i in range(len(rgb)):
+        d.append(dict(rgb=pkt2dict(rgb[i]),
+                      marker=pkt2dict(marker[i]),
+                      torso=pkt2dict(torso[i]),
+                      base=pkt2dict(base[i])))
+
+    np.save(file, arr=d)
 
 
-def ardx2dict(a):
-    d = {}
-    for b in dir(a):
-        if b.startswith('__'):
-            continue
-
-        bb = getattr(a, b)
-
-        if isinstance(bb, (int, float)):
-            d[b] = bb
-
-        else:
-            try:
-                d[b] = ardx.numpy_view(bb)
-            except (TypeError, KeyError):
-                d[b] = ardx2dict(bb)
-
-    return d
-
-
-print(ardx2dict(a=rgb[0]))
-print(ardx2dict(a=marker[0]))
-print(ardx2dict(a=torso[0]))
-
-
+pkt_list2dict_list(file=filepath)
